0fd3c17108fcaa86e68066861b6597b063d8d827
daniel-gomm <daniel.gomm@outlook.de>
2021-05-20 14:23:16 +0200

    Added option to update Elasticsearch

--- python/processing_pipeline/Adapters.py
+++ python/processing_pipeline/Adapters.py
@@ -1,15 +1,19 @@
 import abc
 import os
 from typing import List, Dict
+from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
 
 class Adapter(metaclass=abc.ABCMeta):
     #Adapter to input data
     @classmethod
     def __subclasshook__(cls, subclass):
-        return (hasattr(subclass, 'generate_documents') and callable(subclass.generate_documents) and hasattr(subclass, '__lon__') and callable(subclass.__lon__) or NotImplemented)
+        return (hasattr(subclass, 'generate_documents') and callable(subclass.generate_documents) and hasattr(subclass, '__lon__') and callable(subclass.__lon__) and hasattr(subclass, 'reset') and callable(subclass.reset) or NotImplemented)
 
     @abc.abstractmethod
     def generate_documents(self, no_documents:int) -> List[Dict]:
+        raise NotImplementedError
+    
+    def reset(self):
         raise NotImplementedError
     
     @abc.abstractmethod
@@ -22,6 +26,10 @@ class UnarxiveAdapter(Adapter):
     def __init__(self, folderpath:str):
         self._folderpath = folderpath
         files = os.listdir(folderpath)
+        self._unprocessed_files = list(filter(lambda x: x.endswith(".txt"), files))
+    
+    def reset(self):
+        files = os.listdir(self._folderpath)
         self._unprocessed_files = list(filter(lambda x: x.endswith(".txt"), files))
 
     def generate_documents(self, no_documents: int) -> List[Dict]:
@@ -51,6 +59,10 @@ class UnpaywallAdapter(Adapter):
     def __init__(self, folderpath:str):
         self._folderpath = folderpath
     
+    def reset(self):
+        #TODO: Implement
+        return super().reset()
+    
     def generate_documents(self, no_documents: int) -> List[Dict]:
         #TODO: Implement
         return super().generate_documents(no_documents)
@@ -58,3 +70,36 @@ class UnpaywallAdapter(Adapter):
     def __len__(self) -> int:
         #TODO: Implement
         return super().__len__()
+
+class ElasticsearchAdapter(Adapter):
+
+    def __init__(self, document_store:ElasticsearchDocumentStore, batch_size: int = 10000):
+        self._document_store = document_store
+        self._batch_size = batch_size
+        self._generator = self._document_store.get_all_documents_generator(batch_size = batch_size)
+        self._unprocessed_documents = self._document_store.get_document_count()
+    
+    def reset(self):
+        self._generator = self._document_store.get_all_documents_generator(batch_size = self._batch_size)
+        self._unprocessed_documents = self._document_store.get_document_count()
+    
+    def generate_documents(self, no_documents: int) -> List[Dict]:
+        docs = []
+        while len(docs) < no_documents:
+            new_document = next(self._generator, None)
+            if new_document is None:
+                break
+            docs.append(new_document)
+        self._unprocessed_documents -= len(docs)
+        return docs
+        '''
+        if len(docs) < no_documents:
+            ret = self._unprocessed_documents
+            self._unprocessed_documents = []
+        else:
+            ret = self._unprocessed_documents[:no_documents]
+            self._unprocessed_documents = self._unprocessed_documents[no_documents:]
+        return ret'''
+    
+    def __len__(self):
+        return self._unprocessed_documents
