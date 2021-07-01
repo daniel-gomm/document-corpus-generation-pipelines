01b568c51fff67337f8c9022f482faf0e8bbfd79
daniel-gomm <daniel.gomm@outlook.de>
2021-07-01 13:17:23 +0200

    Added DocumentStore to init

--- kdpipelines/kdpipelines/__init__.py
+++ kdpipelines/kdpipelines/__init__.py
@@ -1,3 +1,4 @@
 from . import Pipeline
 from .pipeline_elements import Adapters, Processors, Sinks
+from .pipeline_elements.document_store import ElasticsearchDocumentStore
 from .database import rocksDB
